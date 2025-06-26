3ef2972915ff03cedd92f96363da32e79601ada0
priyanshu-k712 <pkamani712@gmail.com>
2025-06-26 18:47:49 +0530

    Learning about Updating Data in SQL

--- SQL for Data Science/Updating Data in a Table.sql
+++ SQL for Data Science/Updating Data in a Table.sql
@@ -1,1 +1,13 @@
 USE schooldb;
+SELECT * FROM student;
+UPDATE student SET grade="IX" WHERE grade="9th";
+UPDATE student SET grade="X" WHERE grade="10th";
+UPDATE student SET grade="XI" WHERE grade="11th";
+UPDATE student SET grade="XII" WHERE grade="12th";
+SELECT * FROM student;
+
+UPDATE student SET age=age+1 WHERE age<18;
+SELECT * FROM student;
+
+UPDATE student SET grade="Unknown" WHERE grade IS NULL;
+SELECT * FROM student;
