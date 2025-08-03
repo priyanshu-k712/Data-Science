96ad5f5b3647948bb2dac800555cd13e90bbacd8
priyanshu-k712 <pkamani712@gmail.com>
2025-08-03 20:55:03 +0530

    Finally Training the ML Model

--- Machine Learning/Project_Gurgaon.py
+++ Machine Learning/Project_Gurgaon.py
@@ -5,6 +5,11 @@ from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import root_mean_squared_error
+from sklearn.model_selection import cross_val_score
 
 housing = pd.read_csv('Machine Learning/housing.csv')
 housing['income_cat'] = pd.cut(housing['median_income'],
@@ -41,3 +46,29 @@ full_pipeline = ColumnTransformer([
 
 housing_prepared = full_pipeline.fit_transform(housing)
 print(housing_prepared)
+
+#Linear regression 
+lin_reg = LinearRegression()
+lin_reg.fit(housing_prepared, housing_labels)
+lin_preds = lin_reg.predict(housing_prepared)
+lin_rmse = root_mean_squared_error(housing_labels, lin_preds)
+print(f"RMSE for Linear Regression Model is {lin_rmse}")
+
+
+
+#Decision Tree 
+dec_reg = DecisionTreeRegressor()
+dec_reg.fit(housing_prepared, housing_labels)
+dec_preds = dec_reg.predict(housing_prepared)
+# dec_rmse = root_mean_squared_error(housing_labels, dec_preds)
+dec_rmse = -cross_val_score(dec_reg, housing_prepared, housing_labels, scoring="neg_root_mean_squared_error", cv=10)
+# print(f"RMSE for Decision Tree Model is {dec_rmse}")
+print(pd.Series(dec_rmse).describe())
+
+
+#Decision Tree 
+rf_reg = RandomForestRegressor()
+rf_reg.fit(housing_prepared, housing_labels)
+rf_preds = rf_reg.predict(housing_prepared)
+rf_rmse = root_mean_squared_error(housing_labels, rf_preds)
+print(f"RMSE for Random Forest Model is {rf_rmse}")
