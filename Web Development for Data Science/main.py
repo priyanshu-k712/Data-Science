ab7ea91556c8630d6ede13f1e4e64c97dd3a533d
priyanshu-k712 <pkamani712@gmail.com>
2025-08-20 18:44:42 +0530

    Static and Templets folder

--- Web Development for Data Science/main.py
+++ Web Development for Data Science/main.py
@@ -1,13 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, New World!</p>"
+    return render_template("index.html")
 @app.route("/about")
 def about():
     return "<p>Hello, I am about page!</p>"
+@app.route("/contact")
+def countact():
+    return "<p>Hello, I am contact page!</p>"
 
 
 app.run(debug=True)
