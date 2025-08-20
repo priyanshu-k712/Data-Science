d90a9d3d3833094cad060241933d09fc7bba6a1e
priyanshu-k712 <pkamani712@gmail.com>
2025-08-20 19:19:57 +0530

    Introduction to Flask

--- Web Development for Data Science/Intro/main.py
+++ Web Development for Data Science/Intro/main.py
@@ -1,16 +1,18 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__,
+            static_folder="assets",
+            static_url_path="/files")
 
 @app.route("/")
 def hello_world():
     return render_template("index.html")
 @app.route("/about")
 def about():
-    return "<p>Hello, I am about page!</p>"
+    return render_template("about.html")
 @app.route("/contact")
 def countact():
-    return "<p>Hello, I am contact page!</p>"
+    return render_template("contact.html")
 
 
 app.run(debug=True)
