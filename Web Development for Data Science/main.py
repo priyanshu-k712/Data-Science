b795863bc6c5e380ecbcebfb3c5732270d39424b
priyanshu-k712 <pkamani712@gmail.com>
2025-08-20 18:17:26 +0530

    Basics of Web Dev and Introduction to Flask

---
+++ Web Development for Data Science/main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, New World!</p>"
+@app.route("/about")
+def about():
+    return "<p>Hello, I am about page!</p>"
+
+
+app.run(debug=True)
